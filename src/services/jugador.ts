a38e28ed5da2e4903905a246f46cd36b4358cef6
maryyuu <charigdlopez@gmail.com>
2024-06-14 10:38:18 -0500

    Se agrego Servicios

---
+++ src/services/jugador.ts
@@ -0,0 +1,47 @@
+
+
+import  Jugador  from "../interfaces/jugador.interface";
+import  JugadorModel  from "../models/Jugador";
+
+const upadate =async(id:any, body: Jugador)=>{
+    const itemJugador = await JugadorModel.findOneAndUpdate( {
+        _id:id},body,{
+            new:true,
+        }
+);
+return itemJugador;
+   
+}
+
+const deleteJugador = async (id:any)=>{
+    const eliminandoJugador = await JugadorModel.findOneAndDelete({
+        _id:id
+    },{
+        delete:true
+    }
+)
+return eliminandoJugador
+}
+
+const Optener = async() => {
+    const obtener =  await JugadorModel.find({}).lean()
+    return obtener;
+}
+
+const gettingById= async(id:any)=>{
+    const unJugador = await JugadorModel.findById({
+        _id:id
+    })
+    return unJugador; 
+}
+const guardar = async(body:Jugador) =>{
+    const Guardando = new JugadorModel(body);
+    const save = Guardando.save();
+    return save;
+}
+export {upadate,
+    deleteJugador,
+    Optener,
+    gettingById,
+    guardar
+};
