b6129073f905147a0696cbe5bf0e9467930980ec
[brayan23] <[bdelgadomunoz@gmail.com]>
2024-06-20 17:31:28 -0500

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-
+import cors from 'cors'
 import { router } from "./routes";
 import db from "./config/db";
 const PORT =3001;
@@ -8,6 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(router);
 db()
+
+app.use(cors())
 
 
 app.listen(PORT, () => console.log(`Listo por el puerto ${PORT}`));
