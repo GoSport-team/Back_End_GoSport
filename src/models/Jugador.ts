ce82819a6c90e56e9d2c03c2ec0cef7e4a6cb0eb
maryyuu <charigdlopez@gmail.com>
2024-06-25 21:55:04 -0500

    Nuevos Campos Jugador

--- src/models/Jugador.ts
+++ src/models/Jugador.ts
@@ -2,6 +2,45 @@ import { Schema, model } from "mongoose";
 import InterfaceJugador from "../interfaces/jugador.interface"
 
 const jugadorSchema = new Schema<InterfaceJugador>({
+    nombres:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    correo:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    telefono:{
+        type:Number,
+        require:true,
+        trim:true
+    
+    },
+    genero:{
+          type:String,
+        require:true,
+        trim:true,
+        enum:['Femenino','Masculino','Mixto']
+
+    },
+    urlFoto:{ 
+          type:String,
+        require:true,
+        trim:true
+    },
+    identificacion:{
+        type:String,
+        require:true,
+        trim:true
+    
+    },
+    contrasena:{
+        type:String,
+        require:true,
+        trim:true
+    },
     jornada :{
         type: String,
         require: true,
