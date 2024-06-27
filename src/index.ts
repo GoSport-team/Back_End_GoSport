769bb9632018a9aa8fe6a071e695ba9a26284267
yerson <Dreyerg9@gmail.com>
2024-06-27 13:26:31 -0500

    cambios

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from 'cors'
 import { router } from "./routes";
 import db from "./config/db";
-import cors from 'cors'
 const PORT =3001;
 const app = express();
 
@@ -12,7 +11,7 @@ app.use(router);
 app.use(cors())
 db()
 
-app.use(cors())
+
 
 
 app.listen(PORT, () => console.log(`Listo por el puerto ${PORT}`));
