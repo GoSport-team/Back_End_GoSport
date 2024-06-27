78786c1f9b2d6c82f1606f2e3e3da86dc11c5597
Luisa Fernanda <lufvi02@gmail.com>
2024-06-27 09:28:38 -0500

    solucion problemas CORS

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 import { router } from "./routes";
 import db from "./config/db";
-import cors from 'cors'
-const PORT =3001;
+
+const PORT = 3001;
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(router);
 
-app.use(cors())
-db()
-
-app.use(cors())
-
+db();
 
 app.listen(PORT, () => console.log(`Listo por el puerto ${PORT}`));
