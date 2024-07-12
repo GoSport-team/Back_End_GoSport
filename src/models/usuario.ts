e378ccaf46b2daa7b95518fc8fc5d1d02977e0d7
Luisa Fernanda <lufvi02@gmail.com>
2024-07-12 14:09:26 -0500

    updates ok

--- src/models/usuario.ts
+++ src/models/usuario.ts
@@ -5,23 +5,19 @@ const UsuarioSchema = new Schema<Usuarios>(
   {
     nombres: {
       type: String,
-      trim: true,
       required: true,
     },
     telefono: {
       type: String,
-      trim: true,
       required: true,
     },
     correo: {
       type: String,
-      trim: true,
       required: true,
       unique: true,
     },
     contrasena: {
       type: String,
-      trim: true,
       required: true,
     },
 
@@ -30,39 +26,32 @@ const UsuarioSchema = new Schema<Usuarios>(
     },
     identificacion: {
       type: String,
-      trim: true,
       required: true,
     },
     ficha: {
       type: String,
-      trim: true,
       required: true,
     },
     programa: {
       type: String,
-      trim: true,
     },
     finFicha: {
       type: String,
-      trim: true,
       required: true,
     },
     rol: {
       type: String,
       required: true,
-      trim: true,
       enum: ["jugador", "organizador"],
       default: "jugador",
     },
     jornada: {
       type: String,
       required: true,
-      trim: true,
       enum: ["mañana", "tarde", "noche"],
     },
     estado: {
       type: Boolean,
-      trim: true,
       default: true,
     },
   },
