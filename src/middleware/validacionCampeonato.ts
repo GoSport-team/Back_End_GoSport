3626df59eb9e7e25394816db1a8255247e13ae0f
yesicaz <ymzemanate@soy.sena.edu.co>
2024-07-24 13:13:27 -0500

    cambios

--- src/middleware/validacionCampeonato.ts
+++ src/middleware/validacionCampeonato.ts
@@ -8,20 +8,17 @@ export const validacionCampeonato = async (req: requestExtend, res: Response, ne
         const añoCliente = req.body.añoCreacion
         const tipoCampeonato = req.body.tipoCampeonato
         const existeCampeonato = await CampeonatoModel.findOne({tipoCampeonato: tipoCampeonato, añoCreacion:añoCliente})
-<<<<<<< HEAD
     
         if(existeCampeonato && tipoCampeonato != 'Recreativos' ){
-=======
         console.log(existeCampeonato)
         if(existeCampeonato){
->>>>>>> 9a8b60f793a086a18cc9fe68511dce1e52cce294
             res.status(401).json({
                 msg:`No puedes crear mas de un campeonado de ${tipoCampeonato} en el año ${añoCliente} `
             })
         }else{
             next()
         }
-    } catch (error) {
+    } }catch (error) {
         handleHttp(res, "ERROR", error)
     }
 
