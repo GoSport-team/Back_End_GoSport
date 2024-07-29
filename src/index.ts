e4bd9c12509343bfda0226798f1a23747061fbf1
Luisa Fernanda <lufvi02@gmail.com>
2024-07-29 17:18:14 -0500

    try 2 for deployment

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,28 @@ import { router } from "./routes";
 import db from "./config/db";
 import cookieParser from "cookie-parser";
 import path from "path";
-import 'dotenv/config'
+import 'dotenv/config';
 
-
-
-
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", `http://localhost:${PORT}`],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
-console.log(__dirname)
+console.log(`Current directory: ${__dirname}`);
+console.log(`Configured port: ${PORT}`);
 app.use(router);
 
 db();
 
-app.listen(PORT, () => console.log(`Listo por el puerto ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Listo por el puerto ${PORT}`);
+});
